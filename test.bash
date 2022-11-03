6a8e5b7964ecd6cbdb1ff8703e0c13f5211d8f03
takuma <s21c1006@s.chibakoudai.jp>
2022-11-03 12:08:43 +0900

    test

--- test.bash
+++ test.bash
@@ -14,11 +14,11 @@ out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
-[ "$?" = 1]       || ng ${LINENO}
+[ "$?" = 1 ]       || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo   | ./plus)
-[ "$?" = 1]       || ng ${LINENO}
+[ "$?" = 1 ]       || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
