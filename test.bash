a5386d07a6f720e32bb844d277b2ac5495ea415f
takuma <s21c1006@s.chibakoudai.jp>
2022-12-02 17:51:10 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-FileCopyrightText: 2022 Takuma Abe
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
@@ -14,11 +14,11 @@ out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]       || ng ${LINENO}
+[ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo   | ./plus)
-[ "$?" = 1 ]       || ng ${LINENO}
+[ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
